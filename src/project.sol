3a3829b187b3ca10379119f64c293319d5663ee8
ting kuan su <nn6842620@yahoo.com.tw>
2024-07-19 13:32:41 +0800

    fix logic bug and change strcut

--- src/project.sol
+++ src/project.sol
@@ -1,91 +1,91 @@
 // SPDX-License-Identifier: GPL-3.0
-
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.24;
 
 contract InvestPlatform {
 
-    struct InvestorInformation{
-        address payable name;
-        uint investment;
-    }
-
     struct Investinstruction{
-        address payable adviser;
         address stocksOrganization;
         uint deposit;
         uint investbalance;
         uint investgoal;
         uint date;
-        InvestorInformation[] investorList;
-        bool active; 
+        address payable[] investorList;
+        uint publicPool;
+        bool active;
     }
 
-    event  Deposit(address name, address toaddr, uint balance);
-    event  InstructionSet(address adviser, address instruction);
-    event  Invest(address invsetor, address adviser, uint balance);
-    event  DepositExpire(address adviser);
-    event  Transfer(address from, address to, uint balance); 
-    
-    
-    mapping (address => Investinstruction) public investinstruction;
+    event Deposit(address indexed from, address indexed to, uint256 amount);
+    event InstructionSet(address indexed adviser, address indexed instruction);
+    event Invest(address indexed investor, address indexed adviser, uint256 amount);
+    event DepositExpire(address indexed adviser);
+    event Transfer(address indexed from, address indexed to, uint256 amount);
 
-    receive() external payable { }
+    mapping(address => Investinstruction) public investinstruction;
 
-    //to-do: stocksOrganization replace to a orcale 
+    receive() external payable {}
+
+    //to-do: stocksOrganization replace to a orcale
     function adviserSet(address _stocksOrganization, uint _investBalance, uint _investGoal) external payable{
-        Investinstruction memory instruction;
-        instruction.adviser = payable(msg.sender);
+        Investinstruction storage instruction = investinstruction[msg.sender];
+
         instruction.stocksOrganization = _stocksOrganization;
         instruction.deposit = msg.value;
         instruction.investbalance = _investBalance;
         instruction.investgoal = _investGoal;
         instruction.date = block.timestamp;
         instruction.active = true;
-        investinstruction[msg.sender] = instruction;
+
         emit Deposit(msg.sender, address(this), msg.value);
         emit InstructionSet(msg.sender, address(this));
     }
 
 
-    function invest(address _instruction) external payable{
+    function invest(address _instruction) external payable {
         Investinstruction storage instruction = investinstruction[_instruction];
-        require(instruction.active == true, "this invest project not active yet");
-        //require(instruction.deposit >= (investbalance/10), "deposit not enough for 10% of invest, for ensure invest risk");
-        require(msg.value >= instruction.investbalance, "balance not enough for investment");
+        
+        require(instruction.active, "This investment project is not active");
+        require(msg.value >= instruction.investbalance, "Insufficient balance for investment");
 
-        InvestorInformation memory investorInformation;
-        investorInformation.name = payable(msg.sender);
-        investorInformation.investment = msg.value;
+        instruction.investorList.push(payable(msg.sender));
+        instruction.publicPool += msg.value;
+        instruction.deposit -= instruction.investbalance / 10;
 
-        instruction.investorList.push(investorInformation);
-        instruction.deposit -= instruction.investbalance/10;
-        emit Invest( msg.sender, address(this),msg.value);
-        //check deposit is affordable
-        if (instruction.deposit < instruction.investbalance/10){
+        emit Invest(msg.sender, address(this), msg.value);
+
+        // Check if deposit is sufficient to cover the risk amount
+        if (instruction.deposit < instruction.investbalance / 10) {
             instruction.active = false;
             emit DepositExpire(_instruction);
         }
     }
 
-    function calculateReward(address _instruction, uint profit) external payable{
+    function calculateReward(address _instruction, uint profit) external {
         Investinstruction storage instruction = investinstruction[_instruction];
-        require(msg.sender == instruction.stocksOrganization);
-        require(block.timestamp >= instruction.date + 100000);
-        uint adviserReward = 0;
-        if (profit > instruction.investgoal){
+        address payable adviser = payable(msg.sender);
+        
+        require(msg.sender == instruction.stocksOrganization, "Only stocks organization can calculate rewards");
+        require(block.timestamp >= instruction.date + 100000, "Cannot calculate reward yet");
+
+        if (profit > instruction.investgoal) {
             for (uint i = 0; i < instruction.investorList.length; i++) {
-                instruction.investorList[i].name.transfer(instruction.investgoal);
-                emit Transfer(_instruction, instruction.investorList[i].name, instruction.investgoal);
-                adviserReward += instruction.investorList[i].investment - instruction.investgoal;
+                instruction.investorList[i].transfer(instruction.investgoal);
+                emit Transfer(_instruction, instruction.investorList[i], instruction.investgoal);
+                instruction.publicPool -= instruction.investgoal;
             }
-            instruction.adviser.transfer(adviserReward);
-            emit Transfer(_instruction, instruction.adviser, adviserReward);
-        }else{
+            adviser.transfer(instruction.publicPool);
+            emit Transfer(_instruction, adviser, instruction.publicPool);
+        } else {
+            uint insurance = (instruction.publicPool + instruction.deposit) / instruction.investorList.length;
             for (uint i = 0; i < instruction.investorList.length; i++) {
-                instruction.investorList[i].name.transfer(instruction.investbalance/10);
-                emit Transfer(_instruction, instruction.investorList[i].name, instruction.investbalance/10);
+                instruction.investorList[i].transfer(insurance);
+                emit Transfer(_instruction, instruction.investorList[i], insurance);
             }
         }
+        
         instruction.active = false;
     }
+
+    // function getInvestinstruction(address addr) external view returns (Investinstruction memory) {
+    //     return investinstruction[addr];
+    // }
 }
